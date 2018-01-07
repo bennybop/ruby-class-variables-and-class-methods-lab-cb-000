47c04c1dd8c2b12fc1d4efa92c696131076ac213
bennybop <bennybop74@yahoo.com>
2018-01-07 00:16:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-attr_accessor :name, :artist, :genre 
+attr_accessor :name, :artist, :genre
 @@count = 0
 @@artists = []
 @@genres = []
@@ -9,43 +9,45 @@ def initialize(name, artist, genre)
   @artist = artist
   @genre = genre
   @@count += 1
-  @@genres << genre 
-  @@artists << artist 
+  @@genres << genre
+  @@artists << artist
 end
 
 def self.count
   @@count
 end
 
+def self.genres
+  @@genres.uniq
+end
+
 def self.artists
-  @@artists.uniq 
-end 
+  @@artists.uniq
+end
 
-def self.artist_count
-  artist_count = {}
-  @@artists.each do |artist|
-    if artist_count[artist]
-       artist_count[artist] += 1
-     else
-       artist_count[artist] = 1 
-     end 
-   end
-   artist_count
- end
-
- def self.genres 
-   @@genres.uniq 
- end
-
- def self.genre_count 
+ def self.genre_count
    genre_count = {}
-   @@genres.each do |genre| 
+   @@genres.each do |genre|
      if genre_count[genre]
        genre_count[genre] += 1
-     else 
+     else
        genre_count[genre] = 1
      end
-   end 
+   end
    genre_count
  end     
+
+ 
+ def self.artist_count
+   artist_count = {}
+   @@artists.each do |artist|
+     if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 end
+
